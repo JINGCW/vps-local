e9b86802c6d2ce3688b41e7a6fda6441facc2a2d
JINGCW <1562359870@qq.com>
2019-11-27 21:03:26 +0800

    sudo fix permisson denied err

--- launch.sh
+++ launch.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 port=8080
-target_pid=`lsof -i :$port | awk '{print $2}'|awk 'NR==2{print}'`
+target_pid=`sudo lsof -i :$port | awk '{print $2}'|awk 'NR==2{print}'`
 
 echo running pid is: $target_pid
 if [ -n "$target_pid" ];then
-  kill -9 $target_pid
+  sudo kill -9 $target_pid
 fi
 
-nohup go run up_down_files.go >/dev/null 2>&1 &
+sudo nohup go run up_down_files.go >/dev/null 2>&1 &
 
 echo Succeed!!!
