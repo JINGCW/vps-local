bfb8e05832a3fe67132bd27c44a534deff713854
JINGCW <1562359870@qq.com>
2019-11-27 17:09:32 +0800

    auto kill pid if exist/new run server

--- launch.sh
+++ launch.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+port=8080
+target_pid=lsof -i :$port | awk '{print $2}'|awk 'NR==2{print}'
+
+if [ -n "$pid" ];then
+  kill -9 $pid
+fi
+
 nohup go run up_down_files.go >/dev/null 2>&1 &
 
 echo Succeed!!!
